fe8179d7a62f94f4ed638ee7b918eba9cf2e7455
Alex Lai <tutai0320@gmail.com>
2021-09-28 21:23:35 -0700

    Your comment

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+const CONNECTION_URL = 'mongodb+srv://alextlai:F5Ri5NDEP2LMPz1f@cluster0.dr5lr.mongodb.net/test';
+const PORT = process.env.PORT|| 3000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
